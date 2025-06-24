e04c4be1dedd21f80b314f7fb3b6a227a2a746e4
Jeffrey Paulouse <jeffreypaulousem24@gmail.com>
2025-06-24 15:46:26 +0530

    I have added the banner secction and the slider images in the image folder

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function toggleMenu() {
     icon.classList.add("fa-bars");
   }
 }
+
+  let slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+
+  function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
+
+  function showDivs(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].classList.add("active");
+  }
+  setInterval(() => {plusDivs(1);}, 5000);
